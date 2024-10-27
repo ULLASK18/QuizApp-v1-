ef937d9552ef0ae1f117a8e3413d78b29d835850
ULLAS K <141169279+ULLASK18@users.noreply.github.com>
2024-10-27 21:00:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ const uri = "mongodb+srv://Ullas:Ullas1234@cluster0.6ho9i.mongodb.net/employee?r
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://quiz-app-v1-frontend.vercel.app', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  origin: ['https://quiz-app-v1-frontend.vercel.app'], 
+  methods: ['GET', 'POST'], 
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, 
 }));
