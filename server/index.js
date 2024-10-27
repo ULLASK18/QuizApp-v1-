ade8daa2055400d1bbde02ac68bbacfebc01c96e
ULLASK18 <1jt22cs176@jyothyit.ac.in>
2024-10-27 18:44:57 +0530

    cleared mongoDB issues

--- server/index.js
+++ server/index.js
@@ -10,9 +10,9 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173" }));
 
-mongoose.connect("mongodb://localhost:27017/employee");
+mongoose.connect(process.env.MONGODB_URI);
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
