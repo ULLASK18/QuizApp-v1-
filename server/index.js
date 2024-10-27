7a90a2b7fbbb8ff522f1508cba4f53a53ebc96d7
ULLASK18 <1jt22cs176@jyothyit.ac.in>
2024-10-27 19:18:41 +0530

    cors issues

--- server/index.js
+++ server/index.js
@@ -1,33 +1,51 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 
 const EmployeeModel = require("./models/user");
 
 const app = express();
 const port = process.env.PORT || 5000;
-
+const uri =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://Ullas:Ullas1234@cluster0.6ho9i.mongodb.net/employee";
 
 app.use(express.json());
 
-app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173" }));
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  methods: ["GET", "POST"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);  // Exit the application if connection fails
+  });
+
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
-  EmployeeModel.findOne({ email }).then((user) => {
-    if (user) {
-      if (user.password === password) {
-        res.status(200).json("Success");
-        console.log("Success");
+  EmployeeModel.findOne({ email })
+    .then((user) => {
+      if (user) {
+        if (user.password === password) {
+          res.status(200).json("Success");
+          console.log("Success");
+        } else {
+          res.status(401).json("Password Incorrect!");
+        }
       } else {
-        res.status(401).json("Password Incorrect!");
+        res.status(404).json("User Not Found");
       }
-    } else {
-      res.status(404).json("User Not Found");
-    }
-  }).catch((err) => res.status(500).json({ message: "Server Error", error: err }));
+    })
+    .catch((err) =>
+      res.status(500).json({ message: "Server Error", error: err })
+    );
 });
 
 // Register route
