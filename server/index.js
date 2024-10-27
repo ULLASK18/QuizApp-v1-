755c53e00c50b2d86672585fcf69431dfcf61764
ULLAS K <141169279+ULLASK18@users.noreply.github.com>
2024-10-27 21:56:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,12 +11,7 @@ const uri = "mongodb+srv://Ullas:Ullas1234@cluster0.6ho9i.mongodb.net/employee?r
 
 app.use(express.json());
 
-const corsOptions = {
-  origin: ['https://quiz-app-v1-frontend.vercel.app'],
-  methods: ["GET", "POST"],
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use(cors({ origin: 'https://quiz-app-v1-frontend.vercel.app/' }));
 
 mongoose.connect(uri)
   .then(() => console.log("Connected to MongoDB"))
