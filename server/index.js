9476ef94a731f371fa8c3138a15fe4131b57ece7
ULLAS K <141169279+ULLASK18@users.noreply.github.com>
2024-10-28 20:10:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,11 +11,22 @@ const uri = "mongodb+srv://Ullas:Ullas1234@cluster0.6ho9i.mongodb.net/employee?r
 
 app.use(express.json());
 
-app.use(cors({
-  origin:"https://quiz-app-v1-frontend.vercel.app",
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://quiz-app-v1-frontend.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(204); 
+});
+
+const corsOptions = {
+  origin: "https://quiz-app-v1-frontend.vercel.app",
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type, Authorization",
+  preflightContinue: true, 
+};
+
+app.use(cors(corsOptions));
+
 
 mongoose.connect(uri)
   .then(() => console.log("Connected to MongoDB"))
